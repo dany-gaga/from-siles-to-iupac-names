c098d6652d99dfa177f75c9c68758ffdebd1f7d1
dany-gaga <69458768+dany-gaga@users.noreply.github.com>
2023-03-28 11:08:23 +0100

    Update from_smiles_to_iupacnames.py

--- from_smiles_to_iupacnames.py
+++ from_smiles_to_iupacnames.py
@@ -6,25 +6,23 @@ Created on Tue Mar 28 09:52:15 2023
 @author: dukpe
 """
 
-import pubchempy as pcp
-import os
+# import libraries
+import sys
 import pandas as pd
-
+from itertools import chain
+import pubchempy as pcp
 from pubchempy import Compound, get_compounds
 
-os.chdir('/home/dukpe/Desktop/git repository  forked/')
+# read the csv file
+chem = pd.read_csv(sys.argv[1])
 
-chem = pd.read_csv('predictedpkd_100.csv')
-
-chem1 = chem["SMILES"].tolist()
+chem1 = chem.iloc[:,0].tolist()
 
 ch = []
 for c in chem1:
     c1 = pcp.get_compounds(c, 'smiles')
     ch.append(c1)
-    
-from itertools import chain
-    
+        
 ch1 =  list(chain.from_iterable(ch))
 
 ch2 = []
@@ -32,5 +30,6 @@ for compound in ch1:
     t= compound.iupac_name
     ch2.append(t)
 
-
 chem['compounds'], chem['iupac_name'] = ch1, ch2
+
+chem.to_csv('smiles_iupacname.csv')
