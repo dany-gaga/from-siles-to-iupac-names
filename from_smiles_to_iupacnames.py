15035066e597dc154ebabdb0906719ecb83d3e4b
dany-gaga <69458768+dany-gaga@users.noreply.github.com>
2023-04-12 16:20:49 +0100

    Update from_smiles_to_iupacnames.py

--- from_smiles_to_iupacnames.py
+++ from_smiles_to_iupacnames.py
@@ -26,10 +26,18 @@ for c in chem1:
 ch1 =  list(chain.from_iterable(ch))
 
 ch2 = []
+ch3 = []
+ch4 = []
+
 for compound in ch1:
     t= compound.iupac_name
+    t1= compound.molecular_formula
+    t2= compound.molecular_weight
     ch2.append(t)
+    ch3.append(t1)
+    ch4.append(t2)
 
-chem['compounds'], chem['iupac_name'] = ch1, ch2
+
+chem['compounds'], chem['iupac_name'], chem['mol_weight'], chem['weight'] = ch1, ch2, ch3, ch4
 
 chem.to_csv('smiles_iupacname.csv')
